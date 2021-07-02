d800c92d15fd4b61ed97ecfd6db7b35a6589707b
wxl2 <1263311820@qq.com>
2021-07-02 08:34:38 +0800

    update StoreManger and UserController

--- src/main/java/com/weekpro/mall/controller/UserController.java
+++ src/main/java/com/weekpro/mall/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
 
